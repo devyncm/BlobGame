df3a757fe42f6acb3a8f6167e35f8d57d2773a20
Devyn Melendez <melend53@students.rowan.edu>
2022-06-07 02:32:41 -0400

    Add files via upload

---
+++ Blob Game/TheFinalProject/assets/Editor/CameraFollowEditor.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(CameraFollow))]
+public class CameraFollowEditor : Editor {
+
+	public override void OnInspectorGUI()
+    {
+        DrawDefaultInspector();
+
+        CameraFollow cf = (CameraFollow)target;
+
+        if(GUILayout.Button("Set Min Camera Pos"))
+        {
+            cf.SetMinCamPosition();
+        }
+
+        if (GUILayout.Button("Set Max Camera Pos"))
+        {
+            cf.SetMaxCamPosition();
+        }
+    }
+}
